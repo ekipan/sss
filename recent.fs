b987a5543cdd53fc757a902ecc2f627d339b81fe
ekipan <ekipan@users.noreply.github.com>
2025-12-14 08:53:22 -0500

    line-wrap recent for readability

--- recent.fs
+++ recent.fs
@@ -1,4 +1,6 @@
 \ \ ex: 22 recent ( list 22 words. )
-: id. ( t-) name>string type space ;
+: crtype ( au-) 38 over - $d3 c@ <
+  if cr then type ;
+: id. ( t-) name>string crtype space ;
 :noname ( ut-uf) id. 1- dup 0<> ;
 : recent ( u-) literal dowords drop ;
