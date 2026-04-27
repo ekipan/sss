dcca7c86038fb33b7690659bc4b35ee036424c0a
ekipan <ekipan@users.noreply.github.com>
2026-04-27 11:41:45 -0400

    remove swap and increase genericity
    `tuck` is `swap over`, plus this order suggests extracting the
    phrase `( xt1 xt2 ) $4c over c! 1+ !`, which you might name `(is)` as
    part of implementing ANS Forth `defer`.

--- patch.fs
+++ patch.fs
@@ -2,7 +2,7 @@
 \ "q ." now types "2". the first p is
 \ overwritten w/ a jmp to the second.
 
-: patch:  $4c here ' tuck 1+ ! c! ] ;
+: patch:  here ' $4c over c! 1+ ! ] ;
 \ warning: needs >=3 code bytes!
 \  : bad drop ;   \ inx, rts, \ 2
 \  : victim 1 ;   \ pushone jmp, \ 3
